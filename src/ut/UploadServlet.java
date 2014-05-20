7e7a6f3c0d037e2f9b3e282b158d887b8fda66f7
Atilio <atiliorr@gmail.com>
2014-05-20 22:01:36 +0300

    Final version of the servlet

--- src/ut/UploadServlet.java
+++ src/ut/UploadServlet.java
@@ -3,6 +3,7 @@ package ut;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,126 +17,151 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 public class UploadServlet extends HttpServlet {
+	         
 
 	private static final long serialVersionUID = 1L;
 	private boolean isMultipart;
-   private String filePath;
-   private int maxFileSize = 200 * 1024;
-   private int maxMemSize = 24 * 1024;
-   private File file ;
+	private String filePath;
+	private int maxFileSize = 200 * 1024;
+	private int maxMemSize = 24 * 1024;
+	private File file;
 
-   public void init( ){
-      // Get the file location where it would be stored.
-      filePath = 
-             getServletContext().getInitParameter("file-upload"); 
-   }
-   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
-      // Check that we have a file upload request
-      isMultipart = ServletFileUpload.isMultipartContent(request);
-      response.setContentType("w");
-      java.io.PrintWriter out = response.getWriter( );
-      UploadServlet obj = new UploadServlet();
-      if( !isMultipart ){
-         out.println("<html>");
-         out.println("<head>");
-         out.println("<title>Servlet upload</title>");  
-         out.println("</head>");
-         out.println("<body>");
-         out.println("<p>No file uploaded</p>"); 
-         out.println("</body>");
-         out.println("</html>");
-         return;
-      }
-      DiskFileItemFactory factory = new DiskFileItemFactory();
-      // maximum size that will be stored in memory
-      factory.setSizeThreshold(maxMemSize);
-      // Location to save data that is larger than maxMemSize.
-      factory.setRepository(new File("/home/atilio/temp/temp2/"));
+	public void init() {
+		// Get the file location where it would be stored.
+		filePath = getServletContext().getInitParameter("file-upload");
+	}
 
-      // Create a new file upload handler
-      ServletFileUpload upload = new ServletFileUpload(factory);
-      // maximum file size to be uploaded.
-      upload.setSizeMax( maxFileSize );
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+		// Check that we have a file upload request
+		isMultipart = ServletFileUpload.isMultipartContent(request);
+		response.setContentType("w");
+		java.io.PrintWriter out = response.getWriter();
+		UploadServlet obj = new UploadServlet();
+		if (!isMultipart) {
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet upload</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<p>No file uploaded</p>");
+			out.println("</body>");
+			out.println("</html>");
+			return;
+		}
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		// maximum size that will be stored in memory
+		factory.setSizeThreshold(maxMemSize);
+		// Location to save data that is larger than maxMemSize.
+		factory.setRepository(new File("/home/atilio/temp/temp2/"));
 
-      try{ 
-      // Parse the request to get file items.
-      List fileItems = upload.parseRequest(request);
-	
-      // Process the uploaded file items
-      Iterator i = fileItems.iterator();
+		// Create a new file upload handler
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		// maximum file size to be uploaded.
+		upload.setSizeMax(maxFileSize);
 
-      out.println("<html>");
-      out.println("<head>");
-      out.println("<title>Servlet upload</title>");  
-      out.println("</head>");
-      out.println("<body>");
-      while ( i.hasNext () ) 
-      {
-         FileItem fi = (FileItem)i.next();
-         if ( !fi.isFormField () )	
-         {
-            // Get the uploaded file parameters
-            String fieldName = fi.getFieldName();
-            String fileName = fi.getName();
-            String contentType = fi.getContentType();
-            boolean isInMemory = fi.isInMemory();
-            long sizeInBytes = fi.getSize();
-            // Write the file
-            if( fileName.lastIndexOf("\\") >= 0 ){
-               file = new File( filePath + 
-               fileName.substring( fileName.lastIndexOf("\\"))) ;
-            }else{
-               file = new File( filePath + 
-               fileName.substring(fileName.lastIndexOf("\\")+1)) ;
-            }
-            fi.write( file ) ;
-            
-            //Executing the class with Dalvik VM
-            String fileNameShort = fileName.replaceAll(".java", "");
-            String dir = " /home/ubuntu/android-x86/";
-            out.println("Executing: sh" + "<br>");
-            String command0 = ("sh");
-            String command1 = ("cd" + dir);
-            out.println("Executing: " + command1  + "<br>");
-            String command2 = ("javac " + fileName);
-            out.println("Executing: " + command2  + "<br>");
-            String command3 = ("dx --dex --output = "+ fileNameShort + ".jar  " + fileNameShort + ".class");
-            out.println("Executing: " + command3  + "<br>");
-            String command4 = ("./rund.sh -cp " + fileName + " " + fileNameShort);
-            out.println("Executing: " + command4 + "<br>");
-            String[] commands = {command0, command1,command2,command3,command4};
-            obj.executeCommand(commands);
-   
+		try {
+			// Parse the request to get file items.
+			List fileItems = upload.parseRequest(request);
 
-                
-            out.println("Uploaded Filename: " + fileName + "<br>");
-         }
-      }
-      out.println("</body>");
-      out.println("</html>");
-   }catch(Exception ex) {
-       System.out.println(ex);
-   }
-   }
-   public void doGet(HttpServletRequest request, 
-                       HttpServletResponse response)
-        throws ServletException, java.io.IOException {
-        
-        throw new ServletException("GET method used with " +
-                getClass( ).getName( )+": POST method required.");
-   } 
-   
-   public String executeCommand(String[] command) {
+			// Process the uploaded file items
+			Iterator i = fileItems.iterator();
+
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet upload</title>");
+			out.println("</head>");
+			out.println("<body>");
+			while (i.hasNext()) {
+				FileItem fi = (FileItem) i.next();
+				if (!fi.isFormField()) {
+					// Get the uploaded file parameters
+					String fieldName = fi.getFieldName();
+					String fileName = fi.getName();
+					String contentType = fi.getContentType();
+					boolean isInMemory = fi.isInMemory();
+					long sizeInBytes = fi.getSize();
+					// Write the file
+					if (fileName.lastIndexOf("\\") >= 0) {
+						file = new File(
+								filePath
+										+ fileName.substring(fileName
+												.lastIndexOf("\\")));
+					} else {
+						file = new File(
+								filePath
+										+ fileName.substring(fileName
+												.lastIndexOf("\\") + 1));
+					}
+					fi.write(file);
+
+					// Executing the class with Dalvik VM
+					String fileNameShort = fileName.replaceAll(".java", "");
+					String fileNameJar = (fileNameShort + ".jar");
+					String fileNameClass = (fileNameShort + ".class");
+					String dir = "/home/ubuntu/android-x86/";
+//					String localdir = "/home/atilio/temp/";
+					out.print("G02" + "<br>");
+
+					// Executing javac command
+					String[] command0 = { "sh", "-c", "javac " + dir + fileName };
+					obj.executeCommand(command0);
+					
+
+					// Executing dx command
+					List list0 = new ArrayList();
+					list0.add("dx");
+					list0.add("--dex");
+					list0.add("--output");
+					list0.add(fileNameJar);
+					list0.add(fileNameClass);
+					Process p0 = null;
+					ProcessBuilder pb0 = new ProcessBuilder(list0);
+					pb0.directory(new File(dir));
+					pb0.start();
+					
+
+					// Executing rund.sh command
+					List list1 = new ArrayList();
+					list1.add("./rund.sh");
+					list1.add("-cp");
+					list1.add(fileNameJar);
+					list1.add(fileNameShort);
+					Process p1 = null;
+					ProcessBuilder pb1 = new ProcessBuilder(list1);
+					pb1.directory(new File(dir));
+					pb1.start();
+					
+					out.println("Uploaded Filename: " + fileName + "<br>");
+				}
+			}
+			out.println("</body>");
+			out.println("</html>");
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+
+		throw new ServletException("GET method used with "
+				+ getClass().getName() + ": POST method required.");
+	}
+
+	public String executeCommand(String[] commanda) {
 
 		StringBuffer output = new StringBuffer();
 
 		Process p;
 		try {
-			p = Runtime.getRuntime().exec(command);
+			p = Runtime.getRuntime().exec(commanda);
 			p.waitFor();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                       String line = "";			
-			while ((line = reader.readLine())!= null) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
 				output.append(line + "\n");
 			}
 		} catch (Exception e) {
@@ -143,5 +169,5 @@ public class UploadServlet extends HttpServlet {
 		}
 		return output.toString();
 	}
-	
+
 }
